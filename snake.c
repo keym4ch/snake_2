83d31eaf2ac9cf959eda4f5ede1536749b8c2cb6
keym4ch <macitkinay@gmail.com>
2025-03-04 10:18:42 +0300

    changed gitignore

--- snake.c
+++ snake.c
@@ -5,6 +5,10 @@
 #include <locale.h>
 #include <wchar.h>
 
+#define WALL_COLOR 1
+#define SNAKE_BODY_COLOR 2
+#define PEAR_COLOR 3
+
 //Tried to make unicode characters work so that I could get the head of the snake point to its direction.
 //Will attempt again in the future.
 /*
@@ -27,7 +31,7 @@ struct linked_snake{
     struct linked_snake* link;
 }; typedef struct linked_snake linked_snake;
 
-//Probably didn't need this. But it looks cool. (I think.)
+//Probably didn't need this. 
 struct fruit{
     int xVal;
     int yVal;
@@ -35,8 +39,7 @@ struct fruit{
 }; typedef struct fruit fruit;
 
 //Function that returns the tail in linked_snake type. Note that the function returns the tail of the snake, not the head. 
-//This is to make moving the snake a little more efficient, but also makes the code much less readable. Did this stop me from writing this mess? 
-//Appearently it didn't. I am sorry for all the headache that I might've caused for those who try to read the code. I'll try to make it as readable as possible.    
+//This is to make moving the snake a little more efficient, but also makes the code much less readable.   
 linked_snake* init_snake()
 {
     srand((unsigned int)time(NULL));
@@ -52,7 +55,7 @@ linked_snake* init_snake()
     tail->link->yVal = tail->yVal;
     tail->link->link = (linked_snake*)malloc(sizeof(linked_snake));
     
-    //Creating the body two after the tail, head as of the beginning of the game
+    //Creating the body two after the tail, which is the head as of the beginning of the game
     tail->link->link->xVal = tail->xVal + 2;
     tail->link->link->yVal = tail->yVal;
     tail->link->link->link = NULL;
@@ -64,7 +67,7 @@ linked_snake* init_snake()
 
 
 
-//Function to spawn a 'pear'
+//Function to spawn an 'apple'
 void spawn_pear(linked_snake* tail, fruit* pear)
 {
     int x, y;
@@ -74,14 +77,14 @@ void spawn_pear(linked_snake* tail, fruit* pear)
     x = (rand()%(int)(WIDTH*0.8))+(WIDTH*0.1);
     y = (rand()%(int)(HEIGTH*0.8))+(HEIGTH*0.1);
     
-    //This while loop and the if statement inside checks if the coordinates of the newly spawned 'pear' are already occupied by the snake.
+    //This while loop checks if the coordinates of the newly spawned 'pear' are already occupied by the snake.
     while(traverse != NULL)
     {
         if(x == traverse->xVal && y == traverse->yVal)
         {
             traverse = tail;
-            x = (rand()%(int)(WIDTH*0.8))+(WIDTH*0.1);
-            y = (rand()%(int)(HEIGTH*0.8))+(HEIGTH*0.1);
+            x = (rand()%(int)(WIDTH*0.8)) + (WIDTH*0.1);
+            y = (rand()%(int)(HEIGTH*0.8)) + (HEIGTH*0.1);
         }
         traverse = traverse->link;
     }
@@ -98,7 +101,7 @@ void spawn_pear(linked_snake* tail, fruit* pear)
     linked_snake *newTail = (linked_snake*)malloc(sizeof(linked_snake));
     if(newTail == NULL)
     {
-        fprintf(stdout, "Failed to grow the body: Not enough memory in heap.");
+        fprintf(stdout, "Failed to grow the body: Could not allocate memory from heap.");
         return tail;
     }
     //All this does is to assign the coordinates for the new tail, one 'behind' the previous tail.
@@ -114,7 +117,17 @@ void spawn_pear(linked_snake* tail, fruit* pear)
                 1
 Directions:    2 3
                 4
+
+The game_cycle function is the 'main function' of the game. It has several tasks(in order):
+
+-Iterates through the whole snake starting from tail to head, moving each part of the snake to the next. 
+-Moves the head of the snake according to its direction.
+-Checks if a pear was eaten. If a pear is eaten; spawns another pear, grows the snake and increases the score.
+-Checks if the head of the snake hit the wall.
+-Checks if the snake's head hit its own body. 
 */
+
+
 void game_cycle(linked_snake **tail, int direction, fruit *pear, int *gameOver)
 {
     linked_snake *traverse = (*tail);
@@ -129,7 +142,7 @@ void game_cycle(linked_snake **tail, int direction, fruit *pear, int *gameOver)
     }
     //After the end of the while loop the 'traverse' points to the end of the list or the head of the snake(Remember the list starts from the tail.).
     //I save the head to use for collision & 'pear detection':
-
+    //I didn't save the address of the head outside of the function since each game cycle we iterate through each snake body, which gives us the head.
     linked_snake *head = traverse;
     traverse = (*tail);
 
@@ -153,6 +166,7 @@ void game_cycle(linked_snake **tail, int direction, fruit *pear, int *gameOver)
         (*pear).score++;
     }
     
+    //Detection of whether the head of the snake hits the wall.
     if((head->xVal == 0 || head->xVal == WIDTH - 1 || head->yVal == 0 || head->yVal == HEIGTH -1))
     {
         (*gameOver) = 1;
@@ -174,9 +188,7 @@ void game_cycle(linked_snake **tail, int direction, fruit *pear, int *gameOver)
 }
 
 
-//Notice that if subsequent inputs are received too fast, every input will be queued. 
-//This method relies on the fact that 3 different inputs will be received upon pressing an arrow key. 
-//This is not a good method (I believe) so I will be changing this method to use the keypad() method.
+
 int kbhit (void)
 {
     int ch = getch();
@@ -196,14 +208,21 @@ int kbhit (void)
 
 int main(void)
 {
-    setlocale(LC_ALL, "");
+    
+    //setlocale(LC_ALL, "");
     initscr();
+    
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_WHITE);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_RED, COLOR_BLACK);
+
     noecho();
     cbreak();
     curs_set(0);
     nodelay(stdscr, TRUE);
     scrollok(stdscr, TRUE);
-    wchar_t down[] = {2304, L'\0'};
+    //wchar_t down[] = {2304, L'\0'};
 
 
     linked_snake *tail = init_snake();
@@ -213,7 +232,7 @@ int main(void)
     pear.yVal = 5;
     pear.score = 0;
 
-    int direction = 1, gameOver = 0;
+    int direction = 1, gameOver = 0, napVal;
     
     for(int i = 0; i < 5; i++)
         tail = grow(tail);
@@ -224,6 +243,9 @@ int main(void)
     {
         if(kbhit())
         {
+            //Notice that if subsequent inputs are received too fast, every input will be queued. 
+            //This method relies on the fact that 3 different inputs will be received upon pressing an arrow key. 
+            //This is not a good method (I believe) so I will be changing this method to use the keypad() method.
             if(getch() == '\033')
             {
                 getch();
@@ -269,12 +291,12 @@ int main(void)
         napms(1500);
         */
         
-        
+        //The main game cycle.
         game_cycle(&tail, direction, &pear, &gameOver);
         if(gameOver)
             break;        
 
-
+        attron(COLOR_PAIR(WALL_COLOR));
         for(int i = 0; i < WIDTH; i++)
         {
             mvwaddch(stdscr, 0, i, '#');
@@ -285,34 +307,55 @@ int main(void)
             mvwaddch(stdscr, i, 0, '#');
             mvwaddch(stdscr, i, WIDTH-1, '#');
         }
+        attroff(COLOR_PAIR(WALL_COLOR));
 
-
+        attron(COLOR_PAIR(SNAKE_BODY_COLOR));
         traverse = tail;
-        while(traverse != NULL)
+        while(traverse->link != NULL)
         {
             mvwaddch(stdscr, traverse->yVal, traverse->xVal, 'o');
             traverse=traverse->link;
         }
-    
+        attroff(COLOR_PAIR(SNAKE_BODY_COLOR));
+
+        mvwaddch(stdscr, traverse->yVal, traverse->xVal, 'o');
+
         
+        attron(COLOR_PAIR(PEAR_COLOR));
         mvwaddch(stdscr, pear.yVal, pear.xVal, '+');
+        attroff(COLOR_PAIR(PEAR_COLOR));
         char pearStr[50];
         
-        sprintf(pearStr, "Your score: %d", pear.score);
+        sprintf(pearStr, "Score: %d", pear.score);
         mvwaddstr(stdscr, 0, 60, "-------------------------------");
-        mvwaddstr(stdscr, 1, 70, pearStr);
+        mvwaddstr(stdscr, 1, 72, pearStr);
         mvwaddstr(stdscr, 2, 60, "-------------------------------");
         
-        //Unicode characters don't work as of yet. See the top of the code for more information.
+        //Unicode characters don't work as of yet. See top for more information.
         /*
         mvaddwstr(0, 150, down);
         */
 
 
         refresh();
-        napms(150);
+        
+        if(pear.score <= 60)
+            napVal = 200 - pear.score*2;
+        napms(napVal);
+        
         erase();
     }
+    
+    erase();
+    
+    char scoreStr[15];
+    sprintf(scoreStr, "Score: %d", pear.score);
+    mvwaddstr(stdscr, HEIGTH/2-2, WIDTH/2, "----GAME OVER----");
+    mvwaddstr(stdscr, HEIGTH/2-1, WIDTH/2, "Press q to exit...");
+    mvwaddstr(stdscr, HEIGTH/2, WIDTH/2+4, scoreStr);
+    refresh();
+    
+    while((getch()) != 'q'){}
     
     endwin();
     return (0);
